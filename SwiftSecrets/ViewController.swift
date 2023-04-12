0ba8fb1a3f079d5d557cd8ef55a008d75a17064d
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-04-12 20:54:44 +0500

    Initial commit with gitignore

---
+++ SwiftSecrets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSecrets
+//
+//  Created by Yuri Volegov on 12.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
