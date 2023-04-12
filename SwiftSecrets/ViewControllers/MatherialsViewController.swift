123d0a6c9a2ef74625d567ff7dbeff7927ba5983
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-04-12 21:23:23 +0500

    ViewControllers and Storyboards add

---
+++ SwiftSecrets/ViewControllers/MatherialsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MatherialsViewController.swift
+//  SwiftSecrets
+//
+//  Created by Yuri Volegov on 12.04.2023.
+//
+
+import UIKit
+
+class MatherialsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
