388ecb5c2c5056c08953259b06ab0f7b50f78fcd
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-04-17 09:51:13 +0500

    First swiftSecretsStoryboardAndController setup (find bug with DataStore)

--- SwiftSecrets/ViewControllers/SwiftSecretsListViewController.swift
+++ SwiftSecrets/ViewControllers/SwiftSecretsListViewController.swift
@@ -12,34 +12,27 @@ class SwiftSecretsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return 0
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "secret", for: indexPath)
+        
+        var configuration = cell.defaultContentConfiguration()
+        
+        cell.contentConfiguration = configuration
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -76,14 +69,12 @@ class SwiftSecretsTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
