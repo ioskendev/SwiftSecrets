7c70b2359cd8be93b66e751997429f0106c0be68
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-04-20 15:48:50 +0300

    Matherials Stoaryboard and ViewController base develop

--- SwiftSecrets/ViewControllers/MatherialsViewController.swift
+++ SwiftSecrets/ViewControllers/MatherialsViewController.swift
@@ -12,24 +12,13 @@ final class MatherialsViewController: UIViewController {
     @IBOutlet var matherialImage: UIImageView!
     @IBOutlet var matherialLabel: UILabel!
     
-    var currentSecret: Secret!
+    var secret: Secret!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        matherialImage.image = UIImage(named: currentSecret.image[0])
-        matherialLabel.text = currentSecret.matherials[0]
+        matherialImage.image = UIImage(named: secret.image[0])
+        matherialLabel.text = secret.matherials[0]
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
