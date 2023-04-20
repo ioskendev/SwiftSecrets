3feb283f6657401ac6ed771a193b438c660997e5
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-04-20 11:48:26 +0500

    QuestionsViewController base developing (needs SwiftSecretsViewController refactor and bugfix to run it)

--- SwiftSecrets/ViewControllers/QuestionsViewController.swift
+++ SwiftSecrets/ViewControllers/QuestionsViewController.swift
@@ -9,15 +9,44 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
     
-    var currentSecret: Secret!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var answerButtons: [UIButton]!
+    
+    @IBOutlet var questionButton: UIButton!
+    
+    var secret: Secret!
+    
+    var questionIndex = 0
+    
+    var question: Question {
+        secret.questions[questionIndex]
     }
     
-    // MARK: - Navigation
-
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        secret.result = 0
+        
+        showQuestion()
     }
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        guard questionIndex < secret.questions.count else { return }
+        
+        if sender.tag == question.best {
+            secret.result += 1
+        }
+        
+        questionIndex += 1
+        
+        showQuestion()
+    }
+    
+    func showQuestion() {
+        questionButton.setTitle(question.title, for: .normal)
+        
+        for (button, answer) in zip(answerButtons, question.answers) {
+            button.setTitle(answer, for: .normal)
+        }
+    }
+   
 }
